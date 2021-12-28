636f6dadf95fa123c26fcbc3237ea8fd961fb4c5
calmux <calmuxoto@proton.me>
2021-12-28 23:07:36 +0000

    update defconfigs

---
+++ src/common.h
@@ -0,0 +1,23 @@
+
+#ifndef __COMMON_H__ 
+#define __COMMON_H__
+
+#include <cstdio>
+#include <new>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <assert.h>
+#include <cstdlib>     /* div, div_t */
+#include <cstddef>
+
+#include <boost/filesystem.hpp>
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/positional_options.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/cmdline.hpp>
