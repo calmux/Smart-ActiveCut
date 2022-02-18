f88e0da1f83de5cc4d4eb5382fa971c31daa4413
calmux <calmuxoto@proton.me>
2022-02-18 22:26:40 +0000

    Create Readme file

--- src/grabcutseg.cxx
+++ src/grabcutseg.cxx
@@ -5,4 +5,8 @@
 #include <loadfiles.h>
 #include <query.h>
 
-namespace po = boost::program_optio
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[])
+{
+     std::string input_image_file, init_image_file, alpha_file, tlink_image_file, nlink_image_file, mask_file, gmm_fg_file, gmm_bg_file, priorfg_file, priorbg_file, fg_la
