4b67d1a3c51870b6b01c5ee4bccd963b59c32718
calmux <calmuxoto@proton.me>
2022-02-24 10:46:51 +0000

    fixed testdata

--- src/grabcutseg.cxx
+++ src/grabcutseg.cxx
@@ -9,4 +9,11 @@ namespace po = boost::program_options;
 
 int main(int argc, char* argv[])
 {
-     std::string input_image_file, init_image_file, alpha_file, tlink_image_file, nlink_image_file, mask_file, gmm_fg_file, gmm_bg_file, priorfg_file, priorbg_file, fg_la
+     std::string input_image_file, init_image_file, alpha_file, tlink_image_file, nlink_image_file, mask_file, gmm_fg_file, gmm_bg_file, priorfg_file, priorbg_file, fg_label_file, bg_label_file, score_file, cand_file;
+     
+     // all parameters including GMM.
+     ParType par;
+     unsigned maxem = 0;;
+     unsigned cov_type = 0;
+     bool cov_shared = false;
+     bool noactlearn = fa
