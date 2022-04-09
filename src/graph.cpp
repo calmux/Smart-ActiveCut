34209f0389f5e4fe2875399ef4e97a779e48c104
calmux <calmuxoto@proton.me>
2022-04-09 13:42:16 +0000

    add test example

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,1 +1,11 @@
-/* graph.cp
+/* graph.cpp */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "graph.h"
+
+
+template <typename captype, typename tcaptype, typename flowtype> 
+	Graph<captype, tcaptype, flowtype>::Graph(int n
