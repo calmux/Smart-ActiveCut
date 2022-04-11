34a629e06abca4ce1997e41900d4c88701482f3b
calmux <calmuxoto@proton.me>
2022-04-11 06:27:36 +0000

    Corrected typo in README

--- src/graph.cpp
+++ src/graph.cpp
@@ -8,4 +8,10 @@
 
 
 template <typename captype, typename tcaptype, typename flowtype> 
-	Graph<captype, tcaptype, flowtype>::Graph(int n
+	Graph<captype, tcaptype, flowtype>::Graph(int node_num_max, int edge_num_max, void (*err_function)(char *))
+	: node_num(0),
+	  nodeptr_block(NULL),
+	  error_function(err_function)
+{
+	if (node_num_max < 16) node_num_max = 16;
+	if (edge_num_max < 16) edge_num_max = 1
