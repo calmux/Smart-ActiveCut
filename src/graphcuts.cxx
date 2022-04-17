a09e62e4379ee7b10e5077e055c66c35706b4d46
calmux <calmuxoto@proton.me>
2022-04-17 20:16:27 +0000

    revert last

--- src/graphcuts.cxx
+++ src/graphcuts.cxx
@@ -1,3 +1,11 @@
 #include <common.h>
 #include <graph.h>
-#include <
+#include <gmm.h>
+#include <utility.h>
+
+int build_adjmat(ImageType3DU::Pointer lindexPtr,
+		 const vnl_matrix <double> & data, 
+		 vnl_sparse_matrix <double> & con_map,
+		 ParType & par)
+{
+     ImageType3DU::IndexType linde
