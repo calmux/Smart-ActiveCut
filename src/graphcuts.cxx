44111d4c28dc2d8bdb21e9d8bd3391a9c106639c
calmux <calmuxoto@proton.me>
2022-04-17 21:03:09 +0000

    Update canvas.js

--- src/graphcuts.cxx
+++ src/graphcuts.cxx
@@ -8,4 +8,11 @@ int build_adjmat(ImageType3DU::Pointer lindexPtr,
 		 vnl_sparse_matrix <double> & con_map,
 		 ParType & par)
 {
-     ImageType3DU::IndexType linde
+     ImageType3DU::IndexType lindexIdx, nbr_idx;
+
+     // unsigned n_samples = data.rows();
+     con_map.set_size(par.n_samples, par.n_samples);
+     con_map.clear();
+
+     // define neighborhood iterator
+     typedef itk::ConstantBoundaryCondition<ImageTy
