dbdc24a22274b325a4cd2ac4842bffeda9a30a3a
calmux <calmuxoto@proton.me>
2022-05-10 17:02:45 +0000

    working on docs

--- src/loadfiles.cxx
+++ src/loadfiles.cxx
@@ -117,3 +117,94 @@ int load_priors(ImageType3DU::Pointer lindexPtr,
      ImageType4DF::Pointer priorPtr = priorReader->GetOutput();
      ImageType4DF::SizeType priorSize = priorPtr->GetLargestPossibleRegion().GetSize();
      IteratorType4DF priorIt(priorPtr, priorPtr->GetLargestPossibleRegion());
+     ImageType4DF::IndexType priorIdx;
+
+     for (lindexIt.GoToBegin(); !lindexIt.IsAtEnd(); ++ lindexIt) {
+	  if (lindexIt.Get() > 0) { // in mask
+	       vox_id = lindexIt.Get() - 1;
+	       lindexIdx = lindexIt.GetIndex();
+	       priorIdx[0] = lindexIdx[0];
+	       priorIdx[1] = lindexIdx[1];
+	       priorIdx[2] = lindexIdx[2];
+	       for (priorIdx[3] = 0; priorIdx[3] < priorSize[3]; priorIdx[3] ++) {
+		    priors(vox_id, priorIdx[3]) = priorPtr->GetPixel(priorIdx);
+	       }
+	  } // in mask.
+     }
+
+     // normalize the atlas
+     vnl_vector <double> zero_vec(priors.cols(), 0);
+     for (unsigned n = 0; n < par.n_samples; n ++) {
+	  if (priors.get_row(n).sum() == 0) {
+	       priors.set_row(n, zero_vec);
+	  }
+	  else {
+	       priors.scale_row(n, 1/priors.get_row(n).sum() );
+	  }
+     }
+     return 0;
+}
+
+int align_priors(vnl_matrix<double> & gmm_labels,
+		 const vnl_matrix<double> & priors,
+		 const vnl_vector<unsigned> & alpha,
+		 unsigned whatground,
+		 const ParType & par)
+{
+     unsigned n_comp = 0;
+     if (whatground == ALPHA_FG) n_comp = par.gmm_fg.n_comp;
+     else n_comp = par.gmm_bg.n_comp;
+     std::vector<unsigned> permu_map(n_comp, 0);
+
+     for (unsigned k = 0; k < n_comp; k ++) {
+	  permu_map[k] = k;
+     }
+     
+     sort(permu_map.begin(), permu_map.end() );
+     std::vector<unsigned> permu_map_best(permu_map);
+     unsigned k_new = 0, k_old = 0;
+     unsigned n_matches = 0;
+     unsigned n_matches_best = 0;
+     
+     do {
+	  n_matches = 0;
+	  for (unsigned sample_id = 0; sample_id < gmm_labels.rows(); sample_id ++) {
+     	       if (alpha[sample_id] == whatground) {
+		    k_old = gmm_labels.get_row(sample_id).arg_max();
+		    if (permu_map[k_old] == priors.get_row(sample_id).arg_max()) {
+			 n_matches ++;
+		    }
+	       }
+	  } // sample id
+	  if (n_matches > n_matches_best) {
+	       n_matches_best  = n_matches;
+	       permu_map_best = permu_map;
+	       if (par.verbose >= 2) {
+		    for (unsigned k = 0; k < n_comp; k ++) {
+			 printf("align_priors(): %i->%i, ", k, permu_map[k]);
+		    }
+		    printf("n_matches_best: %i\n", n_matches_best);
+	       } // verbose
+	  }
+     } while (next_permutation(permu_map.begin(), permu_map.end()) );
+
+     if (par.verbose >= 0) {
+	  printf("align_priors(): Best permutation: ");
+	  for (unsigned k = 0; k < n_comp; k ++) {
+	       printf("%i->%i, ", k, permu_map_best[k]);
+	  }
+	  printf("\n");
+     } // verbose
+
+     // update gmm_labels. 
+
+     for (unsigned sample_id = 0; sample_id < gmm_labels.rows(); sample_id ++) {     
+	  if (alpha[sample_id] == whatground) {
+	       k_old = gmm_labels.get_row(sample_id).arg_max();
+	       k_new = permu_map_best[k_old];
+	       gmm_labels(sample_id, k_old) = 0;
+	       gmm_labels(sample_id, k_new) = 1;
+	  }
+     }
+     return 0;
+}
---
+++ src/loadfiles.h
@@ -0,0 +1,6 @@
+int load_data(ImageType4DF::Pointer dataPtr,
+	      ImageType3DI::Pointer maskPtr,
+	      vnl_matrix<double> & D, 
+	      ImageType3DU::Pointer lindexPtr);
+
+int load_cons
