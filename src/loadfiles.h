e39420491961330d4d8aa1835c18d4a5c65b28d0
calmux <calmuxoto@proton.me>
2022-05-10 20:52:22 +0000

    Corrected paths

--- src/loadfiles.h
+++ src/loadfiles.h
@@ -3,4 +3,10 @@ int load_data(ImageType4DF::Pointer dataPtr,
 	      vnl_matrix<double> & D, 
 	      ImageType3DU::Pointer lindexPtr);
 
-int load_cons
+int load_constraints(ImageType3DU::Pointer parcelPtr,
+		     ImageType3DC::Pointer initPtr,
+		     vnl_vector<unsigned> & hard_constraints,
+		     const ParType & par);
+
+int load_priors(ImageType3DU::Pointer parcelPtr,
+		std::string prior
