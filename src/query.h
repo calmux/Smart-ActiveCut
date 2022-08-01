81cda5234dcce2d943d0580b0a51024cbd2e3a58
calmux <calmuxoto@proton.me>
2022-08-01 13:37:53 +0000

    Updated for release

--- src/query.h
+++ src/query.h
@@ -41,3 +41,30 @@ void preprocessingBinaryPredicProb(ImageType3DI::Pointer binaryPredictProb);
 
 void morphologicalProcessingTrueCandidatesVolume(ImageType3DUC::Pointer trueCandidatesVolume,
 						 ImageType3DUC::Pointer trueCandidatesVolumeErosion, 
+						 ImageType3DUC::Pointer trueCandidatesVolumeDilationMinusErosion);
+
+bool computeShapeScoreforCcpCandidates (ImageType3DI::Pointer ccpPredictProb,
+					ImageType3DI::Pointer trimap,
+					const int& numTopCcptoEvaluate,
+					std::vector< std::pair<int, int> >& ccpVectorPairs,
+					ConnectedCompInfor allConnectedComponents[],
+					const float& thresholdScoreforTopCandidates);
+
+
+// return 0 indicates that active learning should stop.
+bool activeLearnCCP(ImageType3DF::Pointer predictProb,
+		   ImageType3DI::Pointer alphaLabel,
+		   ImageType3DC::Pointer trimap,
+		   float thresholdPredictProb,
+		   float thresholdScoreforTopCandidates,
+		   int thresholdCCPVolume);
+
+
+// Read new object's labels, either by self training, or by act-learning, and
+// update the constraints.
+bool update_hardconstraints(const vnl_vector<double> & score_map, 
+			   ImageType3DU::Pointer lindexPtr, 
+			   const vnl_vector<unsigned> & alpha,
+			   ImageType3DC::Pointer init_constraintPtr,
+			   vnl_vector<unsigned> & hard_constraints,
+			   const ParType & par);
---
+++ src/utility.cxx
@@ -0,0 +1,170 @@
+
+// for saving output images. e.g. convert alpha (seg for now) vector to 3D images. and other utility fun.
+
+#include <common.h>
+#include <utility.h>
+
+template <class T>
+int save_asimage(itk::Image<T, 3> ptr, 
+		 vnl_vector<T> array,
+		 std::string fname)
+{
+     
+}
+
+
+int save_image(ImageType3DF::Pointer ptr, std::string filename)
+{
+
+     WriterType3DF::Pointer writer = WriterType3DF::New();
+	  
+     writer->SetInput(ptr);
+     writer->SetFileName(filename);
+     try 
+     { 
+	  writer->Update(); 
+     } 
+     catch( itk::ExceptionObject & err ) 
+     { 
+	  std::cerr << "ExceptionObject caught !" << std::endl; 
+	  std::cerr << err << std::endl; 
+	  return EXIT_FAILURE;
+     } 
+
+     std::cout << "save_gmm_labelmap(): File " << filename << " saved.\n";
+
+     return 0;
+}
+
+
+double compute_beta(const vnl_matrix <double> & data, 
+		    vnl_sparse_matrix <double> & nlinks_map)
+{
+     unsigned n_samples = nlinks_map.rows();
+     double beta_sum = 0, n_edges = 0;
+     for (unsigned sample_idx = 0; sample_idx < n_samples; sample_idx ++) {
+	  for (unsigned nbr_idx = sample_idx; nbr_idx < n_samples; nbr_idx ++) {
+	       if (nlinks_map(sample_idx, nbr_idx) > 0) {
+		    beta_sum += (data.get_row(sample_idx) - data.get_row(nbr_idx)).squared_magnitude();
+		    n_edges ++;
+	       }
+	  }
+     }
+     
+     return 0.5 * (1 / (beta_sum/n_edges) );
+}
+
+int print_par(ParType par)
+{
+     print_gmm(par.gmm_fg);
+     printf("\n");
+     print_gmm(par.gmm_bg);
+     printf("\n");     
+     if (par.verbose >= 1) {
+	  printf("n_channels: %i, n_samples: %i, gamma: %.2f, beta: %E, seed: %i, kmeansruns: %i, verbose: %i\n", par.n_channels, par.n_samples, par.gamma, par.beta, par.seed, par.kmeansruns, par.verbose);
+     }
+	 return 1;
+}
+
+int print_gmm(const GMMType & gmm)
+{
+     unsigned n_channels = gmm.comp[0].mu.size();
+     printf("%s n_comp: %i, n_pts: %i\n", gmm.name.c_str(), gmm.n_comp, gmm.n_pts);
+     // print pi (pi is now different per voxel) so do not print it.
+     // printf("pi = \n");
+     // for (unsigned k = 0; k < gmm.n_comp; k ++) 
+     // 	  printf("%10.2f ", gmm.pi[k]); 
+     // printf("\n");
+
+     printf(" N_k (numPts) = \n");
+     for (unsigned k = 0; k < gmm.n_comp; k ++) 
+     	  printf("%10.2f ", gmm.comp[k].numPts); 
+     printf("\n");
+
+     for (unsigned k = 0; k < gmm.n_comp; k ++) {
+	  // print mu
+	  printf("mu [%i] = \n", k);
+	  for (unsigned i = 0; i < n_channels; i ++) 
+	       printf("%10.2f ", gmm.comp[k].mu[i]);
+	  printf("\n");
+
+
+	  // print cov and inv_cov
+	  printf("cov[%i] and inv_cov[%i] = \n", k, k);
+	  for (unsigned i = 0; i < n_channels; i ++) {
+	       for (unsigned j = 0; j < n_channels; j ++) {
+		    printf("%10.2f ", gmm.comp[k].cov(i,j));
+	       }
+	       printf("\t\t");
+	       for (unsigned j = 0; j < n_channels; j ++) {
+		    printf("%10.2E ", gmm.comp[k].inv_cov(i,j));
+	       }
+	       printf("\n");
+	  }
+     }
+	 return 1;
+}
+
+int print_vnl_matrix(const vnl_matrix<double> & mat, unsigned r, unsigned c)
+{
+
+     for (unsigned ri = 0; ri < r; ri ++) {
+	  for (unsigned ci = 0; ci < c; ci++) {
+	       printf("%f ", mat(ri, ci));
+	  }
+	  printf("\n");
+     }
+     // mat.print(vcl_cout);
+	 return 1;
+}
+
+int print_vnl_vec(const vnl_vector<double> & vec, unsigned N)
+{
+     for (int i = 0; i < N; i++) {
+	  printf("%f ", vec[i]);
+     }
+     printf("\n");
+	 return 1;
+}
+
+int save_gmm_labelmap(vnl_matrix<double> & gmm_labels, // initial gmm labels.
+		      ParType & par,
+		      const vnl_vector<unsigned> & alpha,
+		      unsigned whatground,
+		      ImageType3DU::Pointer lindexPtr,
+		      std::string filename)
+{
+     // define some variables used for initializing output images. 
+     ImageType3DC::IndexType start;
+     start[0] = 0;
+     start[1] = 0;
+     start[2] = 0;
+
+     ImageType3DU::SizeType lindexSize = lindexPtr->GetLargestPossibleRegion().GetSize();
+     ImageType3DC::RegionType volRegion;
+     volRegion.SetSize(lindexSize);
+     volRegion.SetIndex(start);
+
+     // create gmm label image. 
+     ImageType3DS::Pointer labelPtr = ImageType3DS::New();
+     labelPtr->SetRegions(volRegion);
+     labelPtr->Allocate();
+     labelPtr->FillBuffer( 0 ); // init to zero.
+
+     labelPtr->SetOrigin( lindexPtr->GetOrigin() );
+     labelPtr->SetSpacing(lindexPtr->GetSpacing() );
+     labelPtr->SetDirection(lindexPtr->GetDirection() );
+
+     IteratorType3DS labelIt(labelPtr, labelPtr->GetLargestPossibleRegion());
+     IteratorType3DU lindexIt(lindexPtr, lindexPtr->GetLargestPossibleRegion());
+
+     unsigned label = 0, sample_id = 0;
+     for (labelIt.GoToBegin(), lindexIt.GoToBegin(); !lindexIt.IsAtEnd(); ++ lindexIt, ++labelIt) {
+	  if (lindexIt.Get() > 0) {
+	       sample_id = lindexIt.Get() - 1;
+	       if (alpha[sample_id] == whatground) {
+		    label = gmm_labels.get_row(sample_id).arg_max();
+		    labelIt.Set(label + 1); // convert to 1-based.
+	       }
+	  }
+     }
