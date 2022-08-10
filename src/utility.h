dab1a9e1dd754113d0b165da756009c685e3dd77
calmux <calmuxoto@proton.me>
2022-08-10 06:28:16 +0000

    update defconfig

--- src/utility.h
+++ src/utility.h
@@ -1,2 +1,6 @@
 double compute_beta(const vnl_matrix <double> & data, 
-		    vnl_sparse_matrix <double>
+		    vnl_sparse_matrix <double> & nlinks_map);
+int print_par(ParType par);
+int print_gmm(const GMMType & gmm);
+int print_vnl_matrix(const vnl_matrix<double> & mat, unsigned r, unsigned c);
+int print_vnl_vec(const vnl_vector<double> & vec, unsign
