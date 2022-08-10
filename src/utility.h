eeb2d73d32a8f5219b5a46b49fd676c9fd1d0006
calmux <calmuxoto@proton.me>
2022-08-10 20:16:40 +0000

    GIT_SILENT Upgrade KF5 version to 5.69.0.

--- src/utility.h
+++ src/utility.h
@@ -3,4 +3,11 @@ double compute_beta(const vnl_matrix <double> & data,
 int print_par(ParType par);
 int print_gmm(const GMMType & gmm);
 int print_vnl_matrix(const vnl_matrix<double> & mat, unsigned r, unsigned c);
-int print_vnl_vec(const vnl_vector<double> & vec, unsign
+int print_vnl_vec(const vnl_vector<double> & vec, unsigned N);
+
+int save_gmm_labelmap(vnl_matrix<double> & gmm_labels, // initial gmm labels.
+		      ParType & par,
+		      const vnl_vector<unsigned> & alpha,
+		      unsigned whatground,
+		      ImageType3DU::Pointer lindexPtr,
+		
